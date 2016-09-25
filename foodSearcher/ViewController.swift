75459390362c3f099d78826cbd67b3b8da8c44be
Dannyreinhart <Danreinrm@gmail.com>
2016-09-24 22:38:50 -0700

    set up first viewController. Added a label that shows the users current location, and added a button that begins updating a users location. This Doesnt really work, but hey, its a start. :)

--- foodSearcher/ViewController.swift
+++ foodSearcher/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var locationLabel: UILabel!
+    let locationManager = CLLocationManager()
+    let getLocationSpinner = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
 
+    @IBOutlet var confirmLocationButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(getLocationSpinner)
+        getLocationSpinner.center = self.view.center
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func getOrConfirmLocation(sender: AnyObject) {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            getLocationSpinner.startAnimating()
+        }
     }
 
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
 
 }
 
